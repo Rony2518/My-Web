ad30ade61f62bea07c7a70e6e8c740b500e8bd70
Rony2518 <113433588+Rony2518@users.noreply.github.com>
2023-03-01 21:07:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var icon = document.getElementById("icon");
+icon.onclick = function () {
+    document.body.classList.toggle("dark-theme");
+}
+var nav = document.querySelector('nav');
+nav.addEventListener('click', function () {
+    var ul = document.querySelector('nav ul');
+    ul.classList.toggle('show');
+});
+(function () {
+    $('.btn').click(function () {
+        $(this).toggleClass('active');
+        return $('.box').toggleClass('open');
+    });
+
+}).call(this);
